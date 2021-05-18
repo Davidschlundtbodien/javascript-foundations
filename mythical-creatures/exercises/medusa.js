7d05f9cb5c5d101615a8e665f49e8e975cac748b
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-18 10:23:29 -0600

    Complete medusa, person, statue classes with tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = []
+  }
+
+  gazeAtVictim(victim) {
+    if (this.statues.length < 3) {
+      this.statues.push(new Statue(victim.name))
+    } else {
+      var released = this.statues.shift()
+      this.statues.push(new Statue(victim.name))
+       return new Person(released.name, "relieved")
+    }
+  }
+
+}
+
+
+module.exports = Medusa;
